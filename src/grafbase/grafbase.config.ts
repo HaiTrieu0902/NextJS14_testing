cbef285bb6d0882db206117f06f5524bcbd5a1ff
Bùi Hải Triều <trieuql159@gmail.com>
2023-12-14 10:59:17 +0700

    "hih"

--- src/grafbase/grafbase.config.ts
+++ src/grafbase/grafbase.config.ts
@@ -1,48 +1,25 @@
-import { g, config, auth } from '@grafbase/sdk';
-
-// @ts-ignore
-const User = g
-    .model('User', {
-        name: g.string().length({ min: 2, max: 100 }),
-        email: g.string().unique(),
-        avatarUrl: g.url(),
-        description: g.string().length({ min: 2, max: 1000 }).optional(),
-        githubUrl: g.url().optional(),
-        linkedinUrl: g.url().optional(),
-        projects: g
-            .relation(() => Project)
-            .list()
-            .optional(),
-    })
-    .auth((rules) => {
-        rules.public().read();
-    });
-
-// @ts-ignore
-const Project = g
-    .model('Project', {
-        title: g.string().length({ min: 3 }),
-        description: g.string(),
-        image: g.url(),
-        liveSiteUrl: g.url(),
-        githubUrl: g.url(),
-        category: g.string().search(),
-        createdBy: g.relation(() => User),
-    })
-    .auth((rules) => {
-        rules.public().read();
-        rules.private().create().delete().update();
-    });
-
-const jwt = auth.JWT({
-    issuer: 'grafbase',
-    secret: g.env('NEXTAUTH_SECRET'),
-});
+import { graph, config } from '@grafbase/sdk';
+const g = graph.Standalone();
 
 export default config({
-    schema: g,
+    graph: g,
+    // Authentication - https://grafbase.com/docs/auth
     auth: {
-        providers: [jwt],
-        rules: (rules) => rules.private(),
+        // OpenID Connect
+        // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
+        // providers: [oidc],
+        rules: (rules) => {
+            rules.public();
+        },
     },
+    // Caching - https://grafbase.com/docs/graphql-edge-caching
+    // cache: {
+    //   rules: [
+    //     {
+    //       types: ['Query'], // Cache everything for 60 seconds
+    //       maxAge: 60,
+    //       staleWhileRevalidate: 60
+    //     }
+    //   ]
+    // }
 });
